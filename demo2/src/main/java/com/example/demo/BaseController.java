1374e24db990c1c270d0c54790447016c50070b2
Farshid Mehrtash <mehrtash.fm@live.com>
2022-05-18 17:37:51 +0300

    init

---
+++ demo2/src/main/java/com/example/demo/BaseController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public String getMessage1() {
+
+        return "Hello World \n This is Version 1.0.0";
+
+    }
+
+    @GetMapping("/bye")
+    public String getMessage2() {
+
+        return "Goodbye \n This is Version 1.0.0";
+
+    }
+
+
+}
